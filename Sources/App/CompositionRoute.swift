298f9532a220bfc770831477e4ebc6f22aedce81
hogumachu <castle_jun@daum.net>
2023-08-11 21:07:10 +0900

    feat: router setup

--- Sources/App/CompositionRoute.swift
+++ Sources/App/CompositionRoute.swift
@@ -1,8 +1,22 @@
 //
-//  File.swift
+//  CompositionRoute.swift
 //  
 //
 //  Created by 홍성준 on 2023/08/11.
 //
 
-import Foundation
+import Vapor
+
+public enum CompositionRoute {
+    
+    public static func routes(using app: Application) throws {
+        try routeMission(using: app)
+    }
+    
+    static func routeMission(using app: Application) throws {
+        let controller: MissionController = MissionControllerImp()
+        let router: MissionRouter = MissionRouterImp(controller: controller)
+        try router.routes(using: app)
+    }
+    
+}
