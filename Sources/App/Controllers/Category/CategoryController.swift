662447a289706df4bca0fea2ad7c949942a83b3b
hogumachu <castle_jun@daum.net>
2023-08-12 17:09:52 +0900

    feat: add category controller

--- Sources/App/Controllers/Category/CategoryController.swift
+++ Sources/App/Controllers/Category/CategoryController.swift
@@ -1,8 +1,14 @@
 //
-//  File.swift
+//  CategoryController.swift
 //  
 //
 //  Created by 홍성준 on 2023/08/12.
 //
 
-import Foundation
+import Vapor
+
+protocol CategoryController: AnyObject {
+    
+    func categories(using req: Request) async throws -> [Category]
+    
+}
--- Sources/App/Controllers/Category/CategoryControllerImp.swift
+++ Sources/App/Controllers/Category/CategoryControllerImp.swift
@@ -1,8 +1,18 @@
 //
-//  File.swift
+//  CategoryControllerImp.swift
 //  
 //
 //  Created by 홍성준 on 2023/08/12.
 //
 
-import Foundation
+import Fluent
+import Vapor
+
+final class CategoryControllerImp: CategoryController {
+    
+    func categories(using req: Request) async throws -> [Category] {
+        return try await Category.query(on: req.db)
+            .all()
+    }
+    
+}
