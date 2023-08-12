660105adbcc1d00bd67bfc38254b2099969ca64a
hogumachu <castle_jun@daum.net>
2023-08-12 17:10:16 +0900

    feat: add category router

--- Sources/App/Routers/Category/CategoryRouter.swift
+++ Sources/App/Routers/Category/CategoryRouter.swift
@@ -1,8 +1,14 @@
 //
-//  File.swift
+//  CategoryRouter.swift
 //  
 //
 //  Created by 홍성준 on 2023/08/12.
 //
 
-import Foundation
+import Vapor
+
+protocol CategoryRouter: AnyObject {
+    
+    func routes(using app: Application) throws
+    
+}
--- Sources/App/Routers/Category/CategoryRouterImp.swift
+++ Sources/App/Routers/Category/CategoryRouterImp.swift
@@ -1,8 +1,24 @@
 //
-//  File.swift
+//  CategoryRouterImp.swift
 //  
 //
 //  Created by 홍성준 on 2023/08/12.
 //
 
-import Foundation
+import Vapor
+
+final class CategoryRouterImp: CategoryRouter {
+    
+    private let controller: CategoryController
+    
+    init(controller: some CategoryController) {
+        self.controller = controller
+    }
+    
+    func routes(using app: Application) throws {
+        app.get("category") { req async throws -> [Category] in
+            return try await self.controller.categories(using: req)
+        }
+    }
+    
+}
